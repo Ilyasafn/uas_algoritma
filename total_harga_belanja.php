24bddca4a325ddccd39f8929357287b1f3dc17c0
Ilyas Al Furqon <78022243+Ilyasafn@users.noreply.github.com>
2024-03-23 01:31:14 +0800

    blm done

---
+++ total_harga_belanja.php
@@ -0,0 +1,12 @@
+<?php 
+
+$belanjaan = array(
+'baju' => 100000,
+'sepeda' => 1000000,
+'laptop' => 9000000,
+'mouse' => 150000
+);
+
+echo "total pengeluaran saya hari ini sebesar Rp." $belanjaan('laptop');
+
+?>
