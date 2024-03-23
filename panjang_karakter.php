b76ba4799852449928199597959af636982ccc13
Ilyas Al Furqon <78022243+Ilyasafn@users.noreply.github.com>
2024-03-23 08:40:50 +0800

    panjangKarakterDone

--- panjang_karakter.php
+++ panjang_karakter.php
@@ -1,7 +1,8 @@
 <?php 
 
 $nama = "Ilyas Al Furqon";
+$spasi = "";
 
-echo "jumlah karakter dari nama saya $nama adalah".strlen($nama)."karakter."
+echo "jumlah karakter dari nama saya $nama adalah $spasi". strlen($nama) ."$spasi karakter."
 
 ?>
