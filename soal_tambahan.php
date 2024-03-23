c55d669b81c0931b2a72ae1c553ed5a227ba1e12
Ilyas Al Furqon <78022243+Ilyasafn@users.noreply.github.com>
2024-03-23 09:19:57 +0800

    soal_tambahanDone

--- soal_tambahan.php
+++ soal_tambahan.php
@@ -13,4 +13,7 @@ foreach ($A as $piramid) {
     echo "$piramid \n";
 }
 
+//WARNING:
+//code ini saya jalankan pada terminal atau saya melakukan output code ini pada code editor menggunakan extension code runner, maka dari itu jika hasil dari codingan ini dibuka pada browser dan hasil nya tidak sesuai dengan apa yang di minta, maka silakan coba jalankan code pada terminal yang ada pada code editor, itulah kenapa saya tidak pake tag <br> karena jika saya pakai tag <br> pasti tag <br> akan ditampilkan pada terminal code editor begitupun pada seluruh UAS yang saya kerjakan yang terdapat pada folder atau repo ini, Terima kasih.
+
 ?>
