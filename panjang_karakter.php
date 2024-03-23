1e819199f36dd6f220fae30f61159479e479e68b
Ilyas Al Furqon <78022243+Ilyasafn@users.noreply.github.com>
2024-03-23 08:56:12 +0800

    tambahanPanjangKarakter

--- panjang_karakter.php
+++ panjang_karakter.php
@@ -1,7 +1,8 @@
 <?php 
 
 $nama = "Ilyas Al Furqon";
-$spasi = "";
+$spasi = ""; 
+//mohon maaf saya tambah variabel spasi ini untuk merapikan code ketika di jalankan pak, saya cari dimana mana ga ketemu gimana cara ngasih spasi untuk ngehubungin fungsi nya dengan string.
 
 echo "jumlah karakter dari nama saya $nama adalah $spasi". strlen($nama) ."$spasi karakter."
 
