256d2d8bb5ffee663825e06b15456f886e105b44
Ilyas Al Furqon <78022243+Ilyasafn@users.noreply.github.com>
2024-03-23 08:50:12 +0800

    luasLingkaranDone

--- luas_lingkaran.php
+++ luas_lingkaran.php
@@ -1,7 +1,7 @@
 <?php
 
-$jarijari = 5;
-$hasil = pi()*$jarijari *$jarijari; 
+$jarijari = 15;
+$hasil = pi()*$jarijari*$jarijari; 
 
 echo "luas lingkaran dengan jari-jari $jarijari adalah $hasil"
 
