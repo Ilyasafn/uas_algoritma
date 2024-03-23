374200730fccc002b7cd5da853d938883290db85
Ilyas Al Furqon <78022243+Ilyasafn@users.noreply.github.com>
2024-03-23 08:24:56 +0800

    nilairatarataDone

---
+++ nilai_rata_rata_ujian.php
@@ -0,0 +1,13 @@
+<?php 
+
+$nilai = array (
+    'bahasa_indonesia' => 90,
+    'matematika' => 92,
+    'biologi' => 90,
+    'kimia' => 95
+);
+$ratarata = array_sum($nilai) / count($nilai);
+
+echo 'Rata-rata nilai ujian saya adalah' . $ratarata  
+
+?>
